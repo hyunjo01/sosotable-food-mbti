f134ef4187a9ad95a75688dc8cab66ae6cc2cebd
Ujin28 <melon4092@naver.com>
2023-01-09 18:22:18 +0900

    FEAT: calculate point, grade

--- data.js
+++ data.js
@@ -88,98 +88,98 @@ const qnaList = [
 
 const infoList = [
     {
-      from: 0,
-      to: 0,
+      //from: 1,
+      //to: 10,
       name: 'food1',
       desc: 'desc1'
     },
     {
-      from: 1,
-      to: 1,
+      //from: 11,
+      //to: 20,
       name: 'food2',
       desc: 'desc2'
     },
     {
-      from: 2,
-      to: 2,
+      //from: 21,
+      //to: 30,
       name: 'food3',
       desc: 'desc3'
     },
     {
-      from: 3,
-      to: 3,
+      //from: 31,
+      //to: 40,
       name: 'food4',
       desc: 'desc4'
     },
     {
-      from: 4,
-      to: 4,
+      //from: 41,
+      //to: 50,
       name: 'food5',
       desc: 'desc5'
     },
     {
-      from: 5,
-      to: 5,
+      //from: 51,
+      //to: 60,
       name: 'food6',
       desc: 'desc6'
     },
     {
-        from: 6,
-        to: 6,
+        //from: 61,
+        //to: 70,
         name: 'food7',
         desc: 'desc7'
       },
       {
-        from: 7,
-        to: 7,
+        //from: 71,
+        //to: 80,
         name: 'food8',
         desc: 'desc8'
       },
       {
-        from: 8,
-        to: 8,
+        //from: 81,
+        //to: 90,
         name: 'food9',
         desc: 'desc9'
       },
       {
-        from: 9,
-        to: 9,
+        //from: 91,
+        //to: 100,
         name: 'food10',
         desc: 'desc10'
       },
       {
-        from: 10,
-        to: 10,
+        //from: 101,
+        //to: 110,
         name: 'food11',
         desc: 'desc11'
       },
       {
-        from: 11,
-        to: 11,
+        //from: 111,
+        //to: 120,
         name: 'food12',
         desc: 'desc12'
       },
       {
-        from: 12,
-        to: 12,
+        //from: 121,
+        //to: 130,
         name: 'food13',
         desc: 'desc13'
       },
       {
-        from: 13,
-        to: 13,
+        //from: 131,
+        //to: 140,
         name: 'food14',
         desc: 'desc14'
       },
       {
-        from: 14,
-        to: 14,
+        //from: 141,
+        //to: 150,
         name: 'food15',
         desc: 'desc15'
       },
       {
-        from: 15,
-        to: 15,
+        //from: 151,
+        //to: 160,
         name: 'food16',
         desc: 'desc16'
       }
